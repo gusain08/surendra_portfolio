98f204317f7122fb919e94d18b89251ab0da35bb
Surendra Singh <surrigusain8@gmail.com>
2023-06-21 17:42:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import './App.css'
+const App = () => {
+  return (
+  <>
+  <Header/>
+  <section className='main'>
+
+  </section>
+  </>
+  )
+}
+
+export default App
