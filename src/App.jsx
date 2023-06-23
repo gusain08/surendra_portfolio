98810b89625857d672c562df323e4ed10532fd99
Surendra Singh <surrigusain8@gmail.com>
2023-06-23 18:23:40 +0530

    Create Banner OF the Portpolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Header from './components/Header'
+import Banner from './components/Banner'
+import About from './components/About'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 const App = () => {
   return (
   <>
+  
+  <Router>
   <Header/>
-  <section className='main'>
+    <Routes>
+      <Route path={'/'} element={<Banner/>}></Route>
+      <Route path={'/about'} element={<About/>}></Route>
+    </Routes>
+  </Router>
 
-  </section>
   </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './header.css'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     const [toggle, setToggle] = useState(false);
@@ -8,7 +9,8 @@ const Header = () => {
     return (
         <>
             <>
-                <header>
+              <div className="header_wrapper">
+              <header>
                     <button className="hamburger-icon" onClick={()=>setToggle(true)}>
                         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
                     </button>
@@ -31,6 +33,7 @@ const Header = () => {
                         </div>
                     </a>
                 </header>
+              </div>
                 <div className={`nav ${toggle && 'nav-left'}`}>
                     <div className="nav-content">
                         <button className="cross-btn" onClick={()=>setToggle(false)}>
@@ -38,10 +41,12 @@ const Header = () => {
                         </button>
                         <ul>
                             <li>
-                                <a href="#"> Home </a>
+                              
+                                <Link to={'/'}>Home</Link>
                             </li>
                             <li>
-                                <a href="#"> about us </a>
+                             
+                                <Link to={'/about'}>About</Link>
                             </li>
                             <li>
                                 <a href="#"> products </a>
